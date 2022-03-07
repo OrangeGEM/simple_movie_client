beda4c8184049b0a4d2b60b2e8da53ef90285a40
vitalij volkov <vitalij.volkov@itp-it.com>
2022-03-07 17:33:38 +0300

    SN-10 get movies and display ids

---
+++ src/components/MainPage/Movie/index.tsx
@@ -0,0 +1,18 @@
+import React, { useContext, useLayoutEffect, useState } from 'react';
+import { MovieInterface } from '../../../assets/interfaces/movie.interface';
+import { Container } from './styled';
+
+interface Props {
+    movie: MovieInterface;
+}
+
+export default function Movie({ movie }: Props) {
+
+
+    return (
+        <Container>
+            
+        </Container>
+    );
+}
+
